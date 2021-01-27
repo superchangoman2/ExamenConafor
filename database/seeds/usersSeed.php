5650c5b6952455767d827ad2d02404e95338bdd8
superchangoman2 <caor93@gmail.com>
2021-01-27 16:57:11 -0600

    Cambio semilla Usuario

--- database/seeds/usersSeed.php
+++ database/seeds/usersSeed.php
@@ -18,11 +18,11 @@ class usersSeed extends Seeder
           'last_name' => 'ADMIN ADMIN',
           'username' => 'ADMIN',
           'email' => 'ADMIN@ADMIN.COM',
-          'password' => '$2y$12$kLHtOBcb9K3b7q9BwRZG9ufzF/99JsIzBMPHJbFDhBQalHJA66jci',//123
-          'estado' => 'ACTIVO',
+          'password' => '$2y$12$kLHtOBcb9K3b7q9BwRZG9ufzF/99JsIzBMPHJbFDhBQalHJA66jci',
           'rol' =>'ADMINISTRADOR',
+          'estado' => 'ACTIVO',
           ],
-  
+
         ];
   
         DB::table('users')->insert($data);
